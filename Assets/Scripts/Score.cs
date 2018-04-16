b728aa573d807e137bfbaf8bbf8da9ba1d4a569d
MITHUN KHANJIR <Mithun@MITHUN>
2018-04-16 16:15:08 -0400

    Main Menu and other UI
    Contains Main Menu, Pause Menu, other gameplay UI

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour {
+	[SerializeField]
+	private TextMeshProUGUI sscore;
+	private float score;
+	private float previousscore=0;
+
+
+	//private Text sscore;
+
+
+	void Start ()
+	{
+		
+	}
+	
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			score = previousscore + 1;
+			previousscore = score;
+			sscore.text = score.ToString();
+		}
+	}
+}
