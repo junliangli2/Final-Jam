2266b275f66e6ef525bfb14db298d71f35333645
DavidWry <rxw7522@rit.edu>
2018-04-21 19:23:00 -0400

    One idea of AI evasion
    OK, here is a idea of how AI will avoid bullets. It's a simple one. Also I am doing this push to make sure my pushes is OK to everyone. There is too many chaged files in this project.

---
+++ Assets/Scripts/AvoidBullets.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidBullets : MonoBehaviour {
+
+    [SerializeField]
+    float movetime;
+    [SerializeField]
+    int movedirection;
+    int speed;
+
+	// Use this for initialization
+	void Start () {
+        movetime = 0;
+        movedirection = 0;
+        speed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movetime > 0)
+        {
+            movetime -= Time.deltaTime;
+            if (movedirection == 1)
+            {
+                transform.Translate( -200 * Time.deltaTime, 0f, 0f);
+            }
+            else if (movedirection == 2)
+            {
+                transform.Translate( 200 * Time.deltaTime, 0f, 0f);
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Respawn")
+        {
+            //Debug.Log("well");
+            movetime = 0.5f;
+            if (other.transform.position.x > transform.position.x)
+            {
+                movedirection = 1;
+            }
+            else
+            {
+                movedirection = 2;
+            }
+        }
+    }
+}
