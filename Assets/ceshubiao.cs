e03e09c4c7acc858f44bc16da2de0001ab463a51
User <User@DESKTOP-IDFQMUR>
2018-04-23 08:39:36 +0800

    danmu

---
+++ Assets/ceshubiao.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceshubiao : MonoBehaviour
+{
+    public Camera c;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            print(c.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, c.nearClipPlane, Input.mousePosition.y)));
+
+
+        }
+    }
+}
---
+++ Assets/danmu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class danmu : MonoBehaviour
+{
+    public GameObject damu;
+    float p = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+
+            for (int i = 0; i < 25; i++)
+            {
+                Instantiate(damu, this.transform.position, Quaternion.Euler(new Vector3(90, 0, p)));
+                p += 14.4f;
+            }
+
+        }
+    }
+}
